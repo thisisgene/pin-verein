90000b4a18059d8dc27c065c2a95d96fb9b31742
friedrich15 <emdo2000@gmail.com>
2016-07-13 20:13:13 +0200

    Added formspree.io for mail submission.

--- landing/js/script.js
+++ landing/js/script.js
@@ -1,26 +1,27 @@
-// $(function() {
-//     // Get the form.
-//     var form = $('#contact-form');
-//
-//     // Get the messages div.
-//     var formMessages = $('#form-messages');
-//
-//     $(form).submit(function(event) {
-//       event.preventDefault();
-//       var mname = $('#msgname');
-//       var memail = $('#msgemail');
-//       var msg = $('#msgcontent');
-//       var subject = 'Nachricht von ' + mname;
-//
-//       $.ajax({
-//         url: "https://formspree.io/emdo2000@gmail.com",
-//         method: "POST",
-//         data: {
-//           'Name': mname,
-//           'Email': memail,
-//           'Nachricht': msg
-//         },
-//         dataType: "json"
-//       });
-//     });
-// });
+$(function() {
+    // Get the form.
+    var form = $('#contact-form');
+
+    // Get the messages div.
+    var formMessages = $('#form-messages');
+
+    $(form).submit(function(e) {
+    	e.preventDefault();
+    	$.ajax({
+    		url: '//formspree.io/emdo2000@gmail.com',
+    		method: 'POST',
+    		data: $(this).serialize(),
+    		dataType: 'json',
+    		beforeSend: function() {
+    			formMessages.text('Sending...');
+
+    		},
+    		success: function(data) {
+          formMessages.text('Message sent!');
+    		},
+    		error: function(err) {
+    			formMessages.text('Error.' + err);
+    		}
+    	});
+    });
+});
